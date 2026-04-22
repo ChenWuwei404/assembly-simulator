79181ee19e30db93fd1a6edbf56acf5869a01dcf
ChenWuwei404 <uplantting@163.com>
2026-04-22 20:09:53 +0800

    feat: impl `update_commands` for `RAM`

--- src/ram.py
+++ src/ram.py
@@ -6,6 +6,10 @@ class RAM:
     def __init__(self, size: int) -> None:
         self.data = zeros((size), dtype=uint8)
 
+    def update_commands(self, commands: Sequence[Sequence[uint8]]) -> None:
+        for i, command in enumerate(commands):
+            self.data[i*4:(i+1)*4] = command
+
     @overload
     def __getitem__(self, key: int) -> uint8: ...
     @overload
