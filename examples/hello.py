d7955eabc4b95c3c4b7219b45b233c99a05da30f
ChenWuwei404 <uplantting@163.com>
2026-04-24 09:45:47 +0800

    docs: update examples

---
+++ examples/hello.py
@@ -0,0 +1,20 @@
+from runner import run
+
+run(
+    """
+        ASCII 72 - -
+        ASCII 101 - -
+        ASCII 108 - -
+        ASCII 108 - -
+        ASCII 111 - -
+        ASCII 44 - -
+        ASCII 32 - -
+        ASCII 87 - -
+        ASCII 111 - -
+        ASCII 114 - -
+        ASCII 108 - -
+        ASCII 100 - -
+        ASCII 33 - -
+        STOP - - -
+    """
+)
